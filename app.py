368080fa61af565d6944c811f2c65114a0728af9
Daniel Nyvoll <daniel@escali.no>
2024-01-09 14:08:37 +0100

    Fixed pylint

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+"""Flask application instance."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Return a 'Hello, World!' message."""
     return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run()
