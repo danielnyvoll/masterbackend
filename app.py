f04b7cd8aad0f1b307feb5aeee27ecee82ced168
Daniel Nyvoll <daniel@escali.no>
2024-04-16 14:39:51 +0200

    Multiplayer start/stop

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def calculate_distance(pos1, pos2):
 model_save_lock = Lock()
 
 # Ensure these are defined outside your functions to maintain state across calls
-global current_state, command_count, done, train, last_command, last_action
+global current_state, command_count, done, train
+start_model_training = False
 current_state = None
 command_count = 0
 done = False
@@ -43,42 +44,42 @@ multiplayer = False
 
 @socketio.on('send_image')
 def handle_send_image(data):
-    global current_state, prev_ballPosition, prev_playerPosition, command_count, done, train, last_command, last_action_blue, last_action_red, multiplayer
+    global current_state, prev_ballPosition, prev_playerPosition, command_count, done, train, last_command, last_action_blue, last_action_red, multiplayer, start_model_training
+    if(start_model_training):
+        if train:
+            return
 
-    if train:
-        return
+        if not all(key in data for key in ['image', 'playerPosition', 'oppositePlayerPosition', 'ballPosition', 'isGoal']):
+            print("Missing data in request.")
+            return
+        multiplayer = data['isMultiplayer']['multiplayer']
+        next_state = preprocess_image(data['image'])
+        if current_state is not None:
+            
+            if(multiplayer):
+                opponent_distance = calculate_distance(data['oppositePlayerPosition'], data['ballPosition'])
+                opponent_commands = get_available_commands(opponent_distance)
+                red_action = get_action(agentRed, next_state, opponent_commands)  # Adjustable epsilon
 
-    if not all(key in data for key in ['image', 'playerPosition', 'oppositePlayerPosition', 'ballPosition', 'isGoal']):
-        print("Missing data in request.")
-        return
-    multiplayer = data['isMultiplayer']['multiplayer']
-    next_state = preprocess_image(data['image'])
-    if current_state is not None:
-        
+            player_distance = calculate_distance(data['playerPosition'], data['ballPosition'])
+
+            # Define commands based on distances for both players
+            player_commands = get_available_commands(player_distance)
+
+            # Fetch commands for both players
+            blue_action = get_action(agent, next_state, player_commands)  
+            if(multiplayer):
+                emit('command', {'player': player_commands[blue_action], 'opponent': opponent_commands[red_action]})
+            else:
+                emit('command', {'player': player_commands[blue_action], 'opponent': ""})
+
+            update_game_state(data, next_state)
+
+
+        current_state = next_state
         if(multiplayer):
-            opponent_distance = calculate_distance(data['oppositePlayerPosition'], data['ballPosition'])
-            opponent_commands = get_available_commands(opponent_distance)
-            red_action = get_action(agentRed, next_state, opponent_commands)  # Adjustable epsilon
-
-        player_distance = calculate_distance(data['playerPosition'], data['ballPosition'])
-
-        # Define commands based on distances for both players
-        player_commands = get_available_commands(player_distance)
-
-        # Fetch commands for both players
-        blue_action = get_action(agent, next_state, player_commands)  
-        if(multiplayer):
-            emit('command', {'player': player_commands[blue_action], 'opponent': opponent_commands[red_action]})
-        else:
-            emit('command', {'player': player_commands[blue_action], 'opponent': ""})
-
-        update_game_state(data, next_state)
-
-
-    current_state = next_state
-    if(multiplayer):
-        last_action_red = red_action
-    last_action_blue = blue_action
+            last_action_red = red_action
+        last_action_blue = blue_action
 
 def get_action(agent, state, commands ):
     q_values = agent.model.predict(state)[0]
@@ -160,12 +161,32 @@ def load_model_route():
     agent.load_model(os.path.join('model_path', 'model_name.h5'))  # Adjust with your model path and name
     return jsonify({"message": "Model loaded successfully."})
 
+@app.route('/start', methods=['POST'])
+def start_training():
+    global start_model_training
+    start_model_training = True
+    return jsonify({"message": "Training started."})
+
+@app.route('/stop', methods=['POST'])
+def stop_training():
+    global start_model_training, command_count, done, agent, agentRed, multiplayer
+    start_model_training = False
+    done = False
+    command_count = 0
+    socketio.emit('reset',  True)  # Emit reset to the frontend
+    agent.epsilon = 1.0  # Reset epsilon after training stops
+    if multiplayer:
+        agentRed.epsilon = 1.0
+    return jsonify({"message": "Training stopped and model reset."})
+
 def preprocess_image(image_base64, target_size=(10, 10)):
     image_bytes = base64.b64decode(image_base64.split(",")[1])
     image = load_img(BytesIO(image_bytes), target_size=target_size)
     image_array = img_to_array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
     return image_array
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
