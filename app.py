26b372c912aa7c2a8ce4fc40c4435f499209e150
Daniel Nyvoll <daniel@escali.no>
2024-05-12 14:41:53 +0200

    ModelScenario: 2500

--- app.py
+++ app.py
@@ -41,10 +41,15 @@ last_action_red = 0
 last_action_blue = 0
 multiplayer = False
 isMatch = False
+prev_ballDistanceToGoal = 0
+ballDistanceToGoal = 0
+blueGoalPos ={'x': -121/2, 'y': 2.44/2, 'z': 0}
+redGoalPos = {'x': 121/2, 'y': 2.44/2, 'z': 0}
+
 
 @socketio.on('send_image')
 def handle_send_image(data):
-    global current_state, prev_ballPosition, prev_opponent_distance_to_ball, prev_player_distance_to_ball, command_count, done, train, last_command, last_action_blue, last_action_red, multiplayer, start_model_training, isMatch, agent, agentRed
+    global prev_ballDistanceToGoal, ballDistanceToGoal, current_state, prev_ballPosition, prev_opponent_distance_to_ball, prev_player_distance_to_ball, command_count, done, train, last_command, last_action_blue, last_action_red, multiplayer, start_model_training, isMatch, agent, agentRed
     if(start_model_training):
         if train:
             return
@@ -62,14 +67,15 @@ def handle_send_image(data):
                 agent = agentRed
                 player_distance = calculate_distance(data['oppositePlayerPosition'], data['ballPosition'])
                 player_commands = get_available_commands(player_distance)
+                ballDistanceToGoal = calculate_distance(blueGoalPos, data['ballPosition'])
             else:
                 player_distance = calculate_distance(data['playerPosition'], data['ballPosition'])
                 player_commands = get_available_commands(player_distance)
+                ballDistanceToGoal = calculate_distance(redGoalPos,data['ballPosition'])
             if(multiplayer):
                 opponent_distance = calculate_distance(data['oppositePlayerPosition'], data['ballPosition'])
                 opponent_commands = get_available_commands(opponent_distance)
                 red_action = get_action(agentRed, next_state, opponent_commands)
-
             blue_action = get_action(agent, next_state, player_commands)
             if(multiplayer):
                 emit('command', {'player': player_commands[blue_action], 'opponent': opponent_commands[red_action]})
@@ -81,7 +87,7 @@ def handle_send_image(data):
             if not isMatch:
                 update_game_state(data, next_state, goal, scoringSide)
 
-
+        prev_ballDistanceToGoal = ballDistanceToGoal
         current_state = next_state
         prev_player_distance_to_ball = player_distance
         if(multiplayer):
@@ -98,16 +104,16 @@ def get_action(agent, state, commands ):
     return action
 count = 0
 def update_game_state(data, next_state, goal, scoringSide):
-    global command_count, done, train, current_state, last_action_red, last_action_blue, multiplayer, prev_opponent_distance_to_ball, prev_player_distance_to_ball, count
+    global ballDistanceToGoal, prev_ballDistanceToGoal, command_count, done, train, current_state, last_action_red, last_action_blue, multiplayer, prev_opponent_distance_to_ball, prev_player_distance_to_ball, count
     if data['playerPosition'] ==  {'x': 0, 'y': 0, 'z': 0}:
-        rewardBlue, done, _ = get_reward(data['oppositePlayerPosition'], data['ballPosition'], goal, scoringSide, prev_player_distance_to_ball, calculate_distance(data['oppositePlayerPosition'], data['ballPosition']), False)
+        rewardBlue, done, _ = get_reward(data['oppositePlayerPosition'], data['ballPosition'], goal, scoringSide, prev_player_distance_to_ball, calculate_distance(data['oppositePlayerPosition'], data['ballPosition']), False, ballDistanceToGoal, prev_ballDistanceToGoal)
     else:
-        rewardBlue, done, _ = get_reward(data['playerPosition'], data['ballPosition'], goal, scoringSide, prev_player_distance_to_ball, calculate_distance(data['playerPosition'], data['ballPosition']), True)
+        rewardBlue, done, _ = get_reward(data['playerPosition'], data['ballPosition'], goal, scoringSide, prev_player_distance_to_ball, calculate_distance(data['playerPosition'], data['ballPosition']), True, ballDistanceToGoal, prev_ballDistanceToGoal)
 
     agent.add_experience(current_state, last_action_blue, rewardBlue, next_state, done)
 
     if multiplayer:
-        rewardRed, doneRed, _ = get_reward(data['oppositePlayerPosition'], data['ballPosition'], goal, scoringSide, prev_opponent_distance_to_ball, calculate_distance(data['oppositePlayerPosition'], data['ballPosition']), False)
+        rewardRed, doneRed, _ = get_reward(data['oppositePlayerPosition'], data['ballPosition'], goal, scoringSide, prev_opponent_distance_to_ball, calculate_distance(data['oppositePlayerPosition'], data['ballPosition']), False, ballDistanceToGoal, prev_ballDistanceToGoal)
         agentRed.add_experience(current_state, last_action_red, rewardRed, next_state, doneRed)
     command_count += 1
     if (command_count > 68 or done) and not train:
@@ -140,21 +146,29 @@ def get_available_commands(distance):
     if distance < 1:
         return base_commands + ["dribble", "shoot"]
     return base_commands
-
-def get_reward(player_pos, ball_pos, is_goal, scoringSide, prev_ball_distance, current_distance, isBlue):
+def get_reward(player_pos, ball_pos, is_goal, scoringSide, prev_ball_distance, current_distance, isBlue, ballDistanceToGoal, prev_ballDistanceToGoal):
     reward = 0
     if is_goal:
         if (isBlue):
-            reward += 100 * scoringSide
+            reward += 1000 * scoringSide
         else:
-            reward -= 100 * scoringSide
-    elif current_distance < prev_ball_distance:
-        reward += 5
-    elif current_distance == prev_ball_distance:
-        reward -= 20
+            reward -= 1000 * scoringSide
     else:
-        reward -= 10
+        # Reward the player for getting the ball closer to the opponent's goal.
+        if ballDistanceToGoal < prev_ballDistanceToGoal:
+            reward += 25  # increase this value to give more reward
+        elif ballDistanceToGoal > prev_ballDistanceToGoal:
+            reward -= 5   # increase this penalty if moving away should be discouraged more
+
+        if current_distance < prev_ball_distance:
+            reward += 5  # smaller reward for just moving towards the ball
+        elif current_distance > prev_ball_distance:
+            reward -= 10  # smaller penalty for moving away from the ball
+        else:
+            reward -=10
+    print(f"Reward: {reward}, Dist_Ball_Goal: {ballDistanceToGoal}, Prev_Dist_Goal: {prev_ballDistanceToGoal}")
     return reward, is_goal, current_distance
+
 
 VALID_MODEL_NAMES = ['dqn_model_red.keras', 'dqn_model.keras']
 
